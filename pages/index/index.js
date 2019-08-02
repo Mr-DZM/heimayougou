188bb22ff3fcc69982241863f682722abbaffbae
diaozeming <1142377988@qq.com>
2019-08-02 11:35:34 +0800

    首页楼层完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,22 @@
 //Page Object
 Page({
   data: {
-    // 定义一个轮播图数据
+    // 定义一个轮播图数组
     swiperList:[],
+    // 定义一个分类导航数组
+    navCateList:[],
+    // 定义一个楼层数组
+    floorList:[]
+    
   },
   //options(Object)
   onLoad: function(options) {
     // 调用函数 getSwiperList 获得轮播图数据
     this.getSwiperList();
+    // 调用函数 getnavCateList 获取分类导航数据
+    this.getnavCateList();
+    // 调用函数getfloorList获取楼层数据
+    this.getfloorList();
   },
   // 获取轮播图的数据
   getSwiperList(){
@@ -22,6 +31,40 @@ Page({
          });
        }
      });
+  },
+  // 获取分类导航数据
+  getnavCateList(){
+    wx.request({
+      url: "https://api.zbztb.cn/api/public/v1/home/catitems",
+      header: { "content-type": "application/json" },
+      method: "GET",
+      dataType: "json",
+      responseType: "text",
+      success: result => {
+        // console.log(result);
+        // 数据请求回来后更新data里的数据
+        this.setData({
+          navCateList:result.data.message,
+        })
+      },
+
+    });
+      
+  },
+  // 获取楼层数据
+  getfloorList(){
+    wx.request({
+      url: "https://api.zbztb.cn/api/public/v1/home/floordata",
+      success: result => {
+        // console.log(result);
+        // 获取数据后更新数据
+        this.setData({
+          floorList:result.data.message
+        });
+      },
+
+    });
+      
   },
   onReady: function() {
     
